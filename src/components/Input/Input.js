a268d9ed8470e9512ee157369ba32c36af59e7ab
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-04-07 21:08:32 +0600

    Validators

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,9 +1,85 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Input.css";
 
 const Input = ({ input, label }) => {
   const [repeaterForms, setRepeaterForms] = React.useState([]);
-  //   console.log(input);
+  // Validate
+
+  const allPatterns = input?.validate?.split("|");
+
+  const pattern_type = allPatterns?.filter((item) => !item.includes(":"));
+  const length_type = allPatterns?.filter((item) => item.includes(":"));
+
+  let lengthObj = {};
+  let patternObj;
+  let pattern;
+  if (pattern_type?.[0]) {
+    patternObj = pattern_type?.[0];
+
+    if (
+      patternObj == "only_letters" ||
+      patternObj == "only_letter" ||
+      patternObj == "string"
+    ) {
+      pattern = "^[a-zA-Z]+$";
+    } else if (
+      patternObj == "only_numbers" ||
+      patternObj == "only_number" ||
+      patternObj == "integer"
+    ) {
+      pattern = "^[0-9]+$";
+    } else if (
+      patternObj == "only_letters_numbers" ||
+      patternObj == "only_letter_number"
+    ) {
+      pattern = "^[a-zA-Z0-9]+$";
+    } else if (patternObj == "email") {
+      pattern = "^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$";
+    } else if (patternObj == "url") {
+      pattern =
+        "[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)";
+    }
+  }
+  if (length_type?.length) {
+    const length = length_type?.[0]?.split(":");
+    lengthObj[length?.[0] + "Length"] = parseInt(length?.[1]);
+  }
+
+  useEffect(() => {
+    setRepeaterForms([]);
+    input?.type === "repeater" &&
+      input?.value.length &&
+      input?.value?.map((value, i) => {
+        setRepeaterForms((prev) => [
+          ...prev,
+
+          Object.keys(input?.repeater_fields).map((key, index) => {
+            return (
+              <div key={index}>
+                <label
+                  htmlFor={
+                    key + " " + input?.repeater_fields[key]?.html_attr?.id
+                  }
+                >
+                  {input?.repeater_fields[key]?.title}
+                </label>
+                <input
+                  id={key + " " + input?.repeater_fields[key]?.html_attr?.id}
+                  className={input?.repeater_fields[key]?.html_attr?.class}
+                  type={input?.repeater_fields[key]?.type}
+                  name={key}
+                  required={input?.repeater_fields[key]?.required}
+                  defaultValue={input?.value[i]?.[key]}
+                  readOnly={input?.repeater_fields[key]?.readonly}
+                />
+              </div>
+            );
+          }),
+        ]);
+      });
+  }, []);
+
+  //   console.log(repeaterForms);
   const { id, ...html_attr } = input?.html_attr;
 
   delete html_attr?.class;
@@ -27,6 +103,8 @@ const Input = ({ input, label }) => {
               required={input?.repeater_fields[key]?.required}
               defaultValue={input?.repeater_fields[key]?.value}
               readOnly={input?.repeater_fields[key]?.readonly}
+              pattern={pattern}
+              {...lengthObj}
             />
           </div>
         );
@@ -47,6 +125,8 @@ const Input = ({ input, label }) => {
           required={input?.required}
           defaultValue={input?.value}
           readOnly={input?.readonly}
+          pattern={pattern}
+          {...lengthObj}
           {...html_attr}
         />
       ) : input?.type === "radio" ? (
@@ -109,6 +189,8 @@ const Input = ({ input, label }) => {
           required={input?.required}
           defaultValue={input?.value}
           readOnly={input?.readonly}
+          pattern={pattern}
+          {...lengthObj}
           {...html_attr}
         />
       )}
